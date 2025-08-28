3a1615f80f782dde536f12247e82fb4b63811eb9
skhatri <skhatri@microproindia.com>
2025-08-28 11:09:09 +0530

    api

--- index.js
+++ index.js
@@ -15,3 +15,54 @@
 // let l_systemLogGenerator = require('./tasks/systemLogGenerator.js');
 // l_systemLogGenerator();
 
+console.warn('hello')
+
+//let http = require('http');
+// const server = http.createServer((req,resp)=>{
+// resp.writeHead(200,{"content-type":'application/json'})
+//   resp.end(JSON.stringify({
+//     data: 'Hello World!',
+//   }));
+// })
+
+// server.listen(8000)
+
+
+// let server_step = http.createServer((request,response)=>{
+
+//     if(request.url == '/'){
+//             response.writeHead(200,{"content-type":'application/json'})
+
+//     response.end(JSON.stringify({ message: "This is Home Page" }));
+//     }
+    
+//     if(request.url == '/about'){
+//             response.writeHead(200,{"content-type":'application/json'})
+
+//     response.end(JSON.stringify({ message: "This is about Page" }));
+//     }
+// })
+
+
+// server_step.listen(8200);
+
+let http = require('http');
+
+let server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.end(`<html>
+               <head><link rel="stylesheet" href="/style.css"></head>
+               <body><h1>Hello from HTML Page</h1></body>
+             </html>`);
+  } 
+  else if (req.url === "/style.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.end(`body { background: lightblue; text-align:center; font-family:sans-serif; } 
+             h1 { color: darkblue; }`);
+  }
+});
+
+server.listen(8200, () => {
+  console.log("Server running at http://localhost:8200");
+});
